4fd81f46f69371caeff140abc07f012b708596c1
slawkir <slawkir1984@gmail.com>
2021-12-30 20:56:26 +0400

    feat: added solution of Task 1.1.1, Task 1.1.2

---
+++ Task 1/Task 1.1.1 RECTANGLE/Task 1.1.1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task_1._1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sideA;
+            int sideB;
+            
+            do
+            {
+                Console.WriteLine("Enter length of side A: ");
+                sideA = int.Parse(Console.ReadLine());
+                if (sideA <= 0)
+                {
+                    Console.WriteLine("ERROR!!! You entered incorrect data");
+                }
+            } while (sideA <= 0);
+
+            do
+            {
+                Console.WriteLine("Enter length of side B: ");
+                sideB = int.Parse(Console.ReadLine());
+                if (sideB <= 0)
+                {
+                    Console.WriteLine("ERROR!!! You entered incorrect data");
+                }
+            } while (sideB <= 0);
+
+            int areaofRectangle = sideA * sideB;
+            Console.WriteLine("Area of the rectangle equals: {0}", areaofRectangle);
+        }
+    }
+}
