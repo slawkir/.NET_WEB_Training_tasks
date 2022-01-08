7f29a1c9c1f07ee9983572f4dad93e6b4d56c905
slawkir <slawkir1984@gmail.com>
2022-01-08 14:15:47 +0400

    feat: added solution of Task 1.2.4

---
+++ Task 1/Task 1.2/Task 1.2.4. VALIDATOR/Task 1.2.4. VALIDATOR/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Task_1._2._4._VALIDATOR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputSentenses = "";
+            do
+            {
+                Console.WriteLine("Input some sentences (only lowercase letters)");
+                inputSentenses = Console.ReadLine();
+
+                if (!(inputSentenses[inputSentenses.Length - 1] == '.'
+                    || inputSentenses[inputSentenses.Length - 1] == '!'
+                    || inputSentenses[inputSentenses.Length - 1] == '?'))
+                {
+                    Console.WriteLine("The sentence must end with '.', or '!', or '?', or '?!' ");
+                }
+            } while (!(inputSentenses[inputSentenses.Length - 1] == '.'
+                    || inputSentenses[inputSentenses.Length - 1] == '!'
+                    || inputSentenses[inputSentenses.Length - 1] == '?'));
+
+            string[] outputSentences = Regex.Split(inputSentenses, @"([.?!]\s)");
+            for (int i = 0; i < outputSentences.Length; i++)
+            {
+                outputSentences[i] = outputSentences[i].Remove(1, outputSentences[i].Length - 1).ToUpper() + outputSentences[i].Remove(0, 1);
+            }
+
+            foreach (var item in outputSentences)
+            {
+                Console.Write(item);
+            }
+        }
+    }
+}
